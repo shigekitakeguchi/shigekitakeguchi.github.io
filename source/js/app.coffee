0b3a845f30ad207b4a2eb08f3315088e303b0462
shigeki-takeguchi <shigeki.takeguchi@gmail.com>
2015-07-26 22:48:49 +0900

    add

--- source/js/app.coffee
+++
@@ -1,5 +0,0 @@
-(($) ->
-  $(window).on('load', () ->
-
-  )
-)(jQuery)
---
+++ source/js/app.js
@@ -0,0 +1,34 @@
+(function ($){
+  $(function(){
+    $('.itunes-link').each(function(){
+      var id = $(this).data('itunes-id');
+      var element =  $(this);
+      var num = 'resultFunc' + Math.floor(Math.random()* 100);
+      var URL = 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/wa/wsLookup?id='+id+'&country=JP';
+      $.ajax({
+        type: 'GET',
+        url: URL,
+        dataType: 'jsonp',
+        jsonpCallback: num,
+        success: function(json){
+          console.log(json['results'][0]);
+          var htmlElement = '<a href="' + json['results'][0]['trackViewUrl'] + '&at=1010l55r' + '">' + '\n';
+          htmlElement += '<p class="image"><img src="' + json['results'][0]['artworkUrl100'] + '" width="67" height="100"></p>' + '\n';
+          htmlElement += '<div class="textWrap"><p class="trackName">' + json['results'][0]['trackName'] + '</p>' + '\n';
+          htmlElement += '<div class="priceWrap">';
+          if(json['results'][0]['trackHdPrice'] !== undefined){
+            htmlElement += '<p class="price">￥' + json['results'][0]['trackHdPrice'] + ' HD購入</p>' + '\n';
+          }
+          if(json['results'][0]['trackHdRentalPrice'] !== undefined){
+            htmlElement += '<p class="rentalPrice">￥' + json['results'][0]['trackHdRentalPrice'] + ' HDレンタル</p>' + '\n';
+          }
+          htmlElement += '</div>' + '\n';
+          htmlElement += '<p class="longDescription">' + json['results'][0]['longDescription'] + '</p></div>' + '\n';
+
+          htmlElement += '</a>';
+          element.append(htmlElement);
+        }
+      });
+    });
+  });
+})(jQuery);
