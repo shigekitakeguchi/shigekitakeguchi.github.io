b06432c6f8140cb2cd3a7b3572ae238205c54cd5
shigeki-takeguchi <shigeki.takeguchi@gmail.com>
2016-05-16 23:51:32 +0900

    (^o^)

--- source/js/app.js
+++ source/js/app.js
@@ -11,22 +11,29 @@
         dataType: 'jsonp',
         jsonpCallback: num,
         success: function(json){
-          console.log(json['results'][0]);
+          var image170 = json['results'][0]['artworkUrl100'].replace("100x100bb.jpg", "170x170bb-85.jpg", "gi");
+          console.log(image170);
           var htmlElement = '<a href="' + json['results'][0]['trackViewUrl'] + '&at=1010l55r' + '">' + '\n';
-          htmlElement += '<p class="image"><img src="' + json['results'][0]['artworkUrl100'] + '"></p>' + '\n';
+          htmlElement += '<p class="image"><img src="' + image170 + '"></p>' + '\n';
           htmlElement += '<div class="textWrap"><p class="trackName">' + json['results'][0]['trackName'] + '</p>' + '\n';
           htmlElement += '<div class="priceWrap">';
           if(json['results'][0]['trackHdPrice'] !== undefined){
-            htmlElement += '<p class="price">￥' + json['results'][0]['trackHdPrice'] + ' HD購入</p>' + '\n';
+            htmlElement += '<p class="price">￥' + json['results'][0]['trackHdPrice'] + ' <span>HD購入</span></p>' + '\n';
           }
-          if(json['results'][0]['trackHdRentalPrice'] !== undefined){
-            htmlElement += '<p class="rentalPrice">￥' + json['results'][0]['trackHdRentalPrice'] + ' HDレンタル</p>' + '\n';
+          if(json['results'][0]['trackPrice'] !== undefined){
+            htmlElement += '<p class="price">￥' + json['results'][0]['trackPrice'] + ' <span>標準画質購入</span></p>' + '\n';
           }
           htmlElement += '</div>' + '\n';
-          if(json['results'][0]['longDescription'] !== undefined){
-            htmlElement += '<p class="longDescription">' + json['results'][0]['longDescription'] + '</p>' + '\n';
+          htmlElement += '<div class="priceWrap mb15">';
+          if(json['results'][0]['trackHdRentalPrice'] !== undefined){
+            htmlElement += '<p class="rentalPrice">￥' + json['results'][0]['trackHdRentalPrice'] + ' <span>HDレンタル</span></p>' + '\n';
           }
-          htmlElement += '<p><img src="/images/iTunes_Badge_Small_44x15.png"></p>';
+          if(json['results'][0]['trackRentalPrice'] !== undefined){
+            htmlElement += '<p class="rentalPrice">￥' + json['results'][0]['trackRentalPrice'] + ' <span>標準画質レンタル</span></p>' + '\n';
+          }
+          htmlElement += '</div>' + '\n';
+
+          htmlElement += '<p><img src="/images/Get_it_on_iTunes_Badge_US_1114.svg"></p>';
           htmlElement += '</div></a>';
           element.append(htmlElement);
         }
