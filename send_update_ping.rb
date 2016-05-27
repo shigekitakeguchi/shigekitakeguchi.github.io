f21282a947c6513745a759a5c1488b87ed71accc
shigeki-takeguchi <shigeki.takeguchi@dena.com>
2016-05-27 18:48:02 +0900

    (^o^)

--- send_update_ping.rb
+++ send_update_ping.rb
@@ -14,20 +14,32 @@ SITE_NAME = "shigeki.takeguchi.log"              # 自サイト名称
 SITE_URL  = "http://shigekitakeguchi.com"  # 自サイト URL
 # Ping サーバ一覧 ( 以下に送信先を設定する )
 PING_SERVERS = [
-  "http://blog.goo.ne.jp/XMLRPC",
-  "http://blog.with2.net/ping.php",
-  "http://blogsearch.google.co.jp/ping/RPC2",
-  "http://blogsearch.google.com/ping/RPC2",
-  "http://ping.bloggers.jp/rpc/",
-  "http://ping.fc2.com/",
-  "http://ping.feedburner.com",
-  "http://ping.freeblogranking.com/xmlrpc/",
-  "http://ping.rss.drecom.jp/",
-  "http://pingoo.jp/ping/",
-  "http://rpc.pingomatic.com/",
-  "http://rpc.weblogs.com/RPC2",
-  "http://xping.pubsub.com/ping/",
-  "http://blogsearch.google.co.jp/ping/RPC2"
+"http://api.my.yahoo.co.jp/RPC2",
+"http://blog.goo.ne.jp/XMLRPC",
+"http://blogsearch.google.co.jp/ping/RPC2",
+"http://blogsearch.google.com/ping/RPC2",
+"http://ping.blo.gs/",
+"http://ping.bloggers.jp/rpc/",
+"http://ping.blogranking.net/",
+"http://ping.dendou.jp/",
+"http://ping.fc2.com/",
+"http://ping.freeblogranking.com/xmlrpc/",
+"http://pingoo.jp/ping/",
+"http://rpc.weblogs.com/RPC2",
+"http://serenebach.net/rep.cgi",
+"http://rpc.reader.livedoor.com/ping",
+"http://taichistereo.net/xmlrpc/",
+"http://www.i-learn.jp/ping/",
+"http://www.blogpeople.net/servlet/weblogUpdates",
+"http://www.hypernavi.com/ping/",
+"http://rpc.pingomatic.com/",
+"http://ping.speenee.com/xmlrpc",
+"http://ping.feedburner.com/",
+"http://ping.rss.drecom.jp/",
+"http://ranking.kuruten.jp/ping",
+"http://www.blogstyle.jp/",
+"http://xping.pubsub.com/ping/",
+"http://services.newsgator.com/ngws/xmlrpcping.aspx"
 ]
 
 # 処理クラス
